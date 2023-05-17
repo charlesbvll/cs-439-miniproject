c48d69f5ec4ef3358d6f823bea6bba79e311a4e2
Charles Beauville <charles.beauville@epfl.ch>
2023-05-17 14:44:05 +0200

    Update optimizer.py

--- src/model/optimizer.py
+++ src/model/optimizer.py
@@ -2,7 +2,8 @@ import torch
 
 def get(optim_name: str) -> torch.optim:
     if optim_name == "sgd":
-        return sgd()
-
-def sgd():
-    return torch.optim.SGD
+        return torch.optim.SGD
+    elif optim_name == "adam":
+        return torch.optim.Adam
+    else:
+        return torch.optim.RMSprop
